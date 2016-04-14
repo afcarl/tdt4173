19c91e24a35bebb877b3d07ab2cebccdb21b6137
iver56 <iver56@hotmail.com>
2016-04-15 00:06:43 +0200

    Set up some basic code for preprocessing, training and testing

---
+++ assignment5/preprocessing.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding=utf-8
+from PIL import Image
+import os
+import h5py
+import numpy as np
+
+
+class Preprocessing(object):
+    def __init__(self):
+        self.x, self.y = self.get_vectors()
+        self.write_dataset()
+
+    @staticmethod
+    def print_ascii(image):
+        chars = u' ░▒▓█'
+        width, height = image.size
+        pixel_values = list(image.getdata())
+        for y in range(height):
+            r = ''
+            for x in range(width):
+                pixel_value = float(pixel_values[width * y + x]) / 255
+                char_idx = int(round(pixel_value * (len(chars) - 1)))
+                r += chars[char_idx] + chars[char_idx]
+            print r
+
+    @staticmethod
+    def get_vectors():
+        x = []
+        y = []
+        prefix = 'chars74k-lite'
+        characters = 'abcdefghijklmnopqrstuvwxyz'
+
+        for label_index in range(len(characters)):
+            path = os.path.join(prefix, characters[label_index])
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    if file.lower().endswith('.jpg'):
+                        # print file
+
+                        file_path = os.path.join(path, file)
+                        image = Image.open(file_path, 'r')
+                        pixel_values = list(image.getdata())
+                        x.append(pixel_values)
+                        y.append(label_index)
+
+        return x, y
+
+    def write_dataset(self):
+        file_name = 'data_set.hdf5'
+        hdf_file_path = os.path.join('.', file_name)
+        f = h5py.File(hdf_file_path, 'w')
+
+        num_entries = len(self.x)
+        num_training_entries = int(num_entries * 0.8)
+        num_validation_entries = num_entries - num_training_entries
+        vector_size = len(self.x[0])
+
+        print('num_entries', num_entries)
+        print('num_training_entries', num_training_entries)
+        print('num_validation_entries', num_validation_entries)
+        print('input vector size', vector_size)
+
+        training_inputs = np.array(self.x[:num_training_entries]).reshape(
+            (1, num_training_entries, vector_size)
+        )
+        training_targets = np.array(self.y[:num_training_entries]).reshape(
+            (1, num_training_entries)
+        )
+        validation_inputs = np.array(self.x[num_training_entries:]).reshape(
+            (1, num_validation_entries, vector_size)
+        )
+        validation_targets = np.array(self.y[num_training_entries:]).reshape(
+            (1, num_validation_entries)
+        )
+
+        all_inputs = np.array(self.x[:]).reshape(
+            (1, num_entries, vector_size)
+        )
+        all_targets = np.array(self.y[:]).reshape(
+            (1, num_entries)
+        )
+
+        variant = f.create_group('normalized')
+
+        training_group = variant.create_group('training')
+        training_group.create_dataset(name='default', data=training_inputs, compression='gzip')
+        training_group.create_dataset(name='targets', data=training_targets, compression='gzip')
+
+        validation_group = variant.create_group('validation')
+        validation_group.create_dataset(name='default', data=validation_inputs, compression='gzip')
+        validation_group.create_dataset(name='targets', data=validation_targets, compression='gzip')
+
+        all_group = variant.create_group('all')
+        all_group.create_dataset(name='default', data=all_inputs, compression='gzip')
+        all_group.create_dataset(name='targets', data=all_targets, compression='gzip')
+
+        f.close()
+
+
+if __name__ == '__main__':
+    Preprocessing()
---
+++ assignment5/training.py
@@ -0,0 +1,78 @@
+import os
+from sklearn.externals import joblib
+import h5py
+
+with h5py.File('data_set.hdf5', 'r') as hf:
+    X_tr = hf.get('normalized').get('training').get('default')[:][0]
+    Y_tr = hf.get('normalized').get('training').get('targets')[:][0]
+    X_va = hf.get('normalized').get('validation').get('default')[:][0]
+    Y_va = hf.get('normalized').get('validation').get('targets')[:][0]
+
+USE_EXISTING_CLASSIFIER = False
+STORE_CLASSIFIER = True
+CLASSIFIER_TYPE = 'svc'  # used for training
+
+classifier_file_path = os.path.join('classifiers', CLASSIFIER_TYPE + '_classifier.pickle')
+
+if USE_EXISTING_CLASSIFIER and os.path.exists(classifier_file_path):
+    print 'Using existing classifier'
+    classifier = joblib.load(classifier_file_path)
+else:
+    print 'Training...'
+    if CLASSIFIER_TYPE == 'svc':
+        from sklearn.svm import SVC
+
+        classifier = SVC(kernel='linear', C=1.0)
+    elif CLASSIFIER_TYPE == 'random_forest':
+        from sklearn.ensemble import RandomForestClassifier
+
+        classifier = RandomForestClassifier(n_estimators=26)
+    elif CLASSIFIER_TYPE == 'nearest_neighbour':
+        from sklearn import neighbors
+
+        classifier = neighbors.KNeighborsClassifier(n_neighbors=5, weights='uniform')
+    elif CLASSIFIER_TYPE == 'ridge_regression':
+        from sklearn import linear_model
+
+        classifier = linear_model.Ridge(alpha=0.5)
+    else:
+        raise Exception('CLASSIFIER variable has an invalid value')
+    classifier.fit(X_tr, Y_tr)
+    if STORE_CLASSIFIER:
+        joblib.dump(classifier, classifier_file_path)
+
+print 'Classifier:', classifier
+
+
+def test_performance(x, y):
+    y_predicted = classifier.predict(x)
+
+    num_predicted = len(y_predicted)
+    num_correct_classifications = 0
+
+    for i in range(len(y_predicted)):
+        y_predicted[i] = int(round(y_predicted[i]))
+        """
+        print '======='
+        print '  correct classification:', y[i]
+        print 'predicted classification:', y_predicted[i]
+        """
+
+        if y[i] == y_predicted[i]:
+            num_correct_classifications += 1
+
+    print '========================================================='
+    int_format = '{0: >50}: {1}'
+    percentage_format = '{0: >50}: {1:.1f} %'
+
+    print int_format.format('Total number of items classified', num_predicted)
+    print percentage_format.format(
+            'Correct classifications (percentage)',
+            100 * float(num_correct_classifications) / num_predicted
+    )
+
+print 'Testing performance on validation set...'
+test_performance(X_va, Y_va)
+print
+print 'Testing performance on training set...'
+test_performance(X_tr, Y_tr)
